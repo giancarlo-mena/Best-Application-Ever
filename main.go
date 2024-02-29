acc7d3aa1a3a298eb5ff35303e6e194b5feb252f
Giancarlo Mena <giancarlo.mena@outlook.com>
2024-02-29 20:06:18 -0300

    enhanced html and added htmx to it

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"html/template"
-	"math/rand"
 	"net/http"
 )
 
 type PageData struct {
 	PageTitle   string
 	Description string
-	Answer      string
-}
-
-func randRange(min, max int) int {
-	return rand.Intn(max-min) + min
 }
 
 func main() {
@@ -22,7 +16,6 @@ func main() {
 		data := PageData{
 			PageTitle:   "Guessing Game",
 			Description: "Guess a number between 1 and 6.",
-			Answer:      "Hello World!",
 		}
 		tmpl.Execute(w, data)
 	})
